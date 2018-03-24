bc4c04e58b9d2580d59e018fe85dd516e7f922e8
Karpukhin Pavel <ruberoid55@gmail.com>
2018-03-24 21:31:41 +0600

    Добавлено деление на правую и левую последовательность бит открытого текста после перемешивания

--- src/com/company/Des.java
+++ src/com/company/Des.java
@@ -1,25 +1,66 @@
 package com.company;
 
 public class Des {
-    private long key;
-    private long text;
+
 
     public Des (long text, long key){
-        //text = reshuffleText(text);
-        reshuffleText(text);
-        this.text = text;
-        this.key = key;
+        long openText = reshuffleText(text);
+        System.out.println();
+        long left = getLeft(openText);
+        long right = getRight(openText);
+    }
+
+    /**
+     * Получение левой последовательности бит после начальной перестановки открытого текста
+     * @param openText
+     * @return левая последовальность бит после начальной перестановки открытого текста
+     */
+    private long getLeft(long openText){
+        long right =  0;
+
+        for (int i = 63; i>=32; i--){
+            byte bit = (byte)getBit(openText, i);
+            System.out.print(bit);
+            if (bit == 1){
+                right = setBit(right, i);
+            } else{
+                right = clearBit(right, i);
+            }
+        }
+
+        return right;
+    }
+
+    /**
+     * Получение правой последовательности бит после начальной перестановки открытого текста
+     * @param openText
+     * @return правая последовальность бит после начальной перестановки открытого текста
+     */
+    private long getRight(long openText){
+        long right =  0;
+
+        for (int i = 31; i>=0; i--){
+            byte bit = (byte)getBit(openText, i);
+            System.out.print(bit);
+            if (bit == 1){
+                right = setBit(right, i);
+            } else{
+                right = clearBit(right, i);
+            }
+        }
+
+        return right;
     }
 
 
     /**
      * Начальная перестановка открытого текста
-     *
      * @param openText
-     * @return возвращает перемешанную последовательность бит согласно матрицe начальной перестановк IP
+     * @return возвращает перемешанную последовательность бит согласно матрице начальной перестановки IP
      */
     private long reshuffleText(long openText){
         long newText = 0;
+
         for (int i = 63; i >= 0; i--){
             byte bit = (byte)getBit(openText, Tables.IP[i]-1);
             System.out.print(bit);
@@ -29,23 +70,22 @@ public class Des {
                 newText = clearBit(newText, i);
             }
         }
+
         return newText;
     }
 
     /**
      * Получениие бита числа в указанную позиции
-     *
      * @param number
      * @param position
      * @return бит числа в указанной позиции
      */
     private long getBit (long number, long position){
-        return (number >> position) & 1;
+        return (number >> position) & 1L;
     }
 
     /**
      * Устанавливка бита в указанную позицию
-     *
      * @param number
      * @param position
      * @return число с измененным битом в указанной позиции
@@ -57,7 +97,6 @@ public class Des {
 
     /**
      * Обнуление бита в указанной позиции
-     *
      * @param number
      * @param position
      * @return число с обнуленным битом в указанной позиции
@@ -71,11 +110,8 @@ public class Des {
 
 
     public static void main(String[] args) {
-        String openText = "10101001 11000110 10010100 00100110 10010000 01010010 00010101 00001100";
         long opT =0b1010100111000110100101000010011010010000010100100001010100001100L;
-        String key = "1010101 0001101 1010011 1111111 1010101 1010101 0001111 1101010";
         long ke = 0b10101010001101101001111111111010101101010100011111101010L;
         Des des = new Des(opT, ke);
-        //des.algorithmDes();
     }
 }
